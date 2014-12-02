8f472f1e3b63bdf759a75bd08fc4fea8c236fd7e
KalleV <kvirtaneva@gmail.com>
2014-12-02 15:42:03 -0700

    continuing to try to unit test tile_directive

--- test/unit/grid/tile_directive_spec.js
+++ test/unit/grid/tile_directive_spec.js
@@ -6,35 +6,44 @@
 
     var ngModel,
         html,
+        $rootScope,
         element,
+        $element,
         scope,
         tile;
 
     beforeEach(module('Grid'));
 
-    beforeEach(inject(function($httpBackend, $compile, $rootScope) {
+    beforeEach(inject(function($httpBackend, $compile, _$rootScope_) {
+      $rootScope = _$rootScope_;
       $httpBackend.whenGET('scripts/grid/tile.html')
         .respond('<div ng-if="ngModel" class="tile position-{{ngModel.x}}-{{ngModel.y}} tile-{{ngModel.value}}">' +
                     '<div class="tile-inner">' +
                       '{{ngModel.value}}' +
                     '</div>' +
                  '</div>');
+      scope = $rootScope.$new();
 
       //ngModel = {x: 0, y: 0, value: 2};
       tile = {x: 0, y: 0, value: 2};
-      //html = '<div tile></div>';
-      html = '<div tile ng-model="tile"></div>';
-            //'ng-repeat="tile in ngModel.tiles track by $index"></div>';
 
-             //'<div ng-if="ngModel" class="tile position-{{ngModel.x}}-{{ngModel.y}} tile-{{ngModel.value}}">' +
-             //   '<div class="tile-inner">' +
-             //   '</div>' +
-             //'</div>';
-      scope = $rootScope.$new();
-      scope.ngModel = {};//ngModel;
-      scope.tile = tile;
-      element = $compile(html)(scope);
-      scope.$digest();
+//      ngModel = {x: 0, y: 0, value: 2};
+      $rootScope.ngModel = tile;
+
+      html = '<div tile ng-model="tile"></div>';
+      $element = angular.element(html);
+      console.log(angular.element('<div>name</div>')[0]);
+
+      //html = '<div tile></div>';
+
+      //html = '<div tile ng-model="tile"></div>';
+      //$rootScope.ngModel = ngModel;
+
+      //scope.ngModel = tile;
+      element = $compile($element)(scope); //$rootScope);
+      $rootScope.$apply();
+      //$rootScope.$digest();
+
     }));
     //http://angular-tips.com/blog/2014/06/introduction-to-unit-test-directives/
     //http://www.ng-newsletter.com/posts/beginner2expert-directives.html
@@ -42,12 +51,14 @@
     //
     //https://egghead.io/lessons/angularjs-unit-testing-directive-scope
     //http://stackoverflow.com/questions/17371836/how-to-unit-test-isolated-scope-directive-in-angularjs
-    it('assigns the tile to the directive\'s ngModel in the inner scope', function() {
-
+    it('assigns the tile to the directive\'s ngModel in the directive\'s inner scope', function() {
+//      console.log(element.scope().ngModel);
+      console.log($element.isolateScope());
+      expect(element.scope().ngModel).toBeDefined();
     });
 
     it('adds the x, y, and value properties to the class names', function() {
-      console.log(angular.element(element), scope);
+      //console.log(angular.element(element), scope);
       expect(angular.element(element.getElementsByClassName('tile position-0-0 tile-2'))).toBe(1);
       console.log(element.isolateScope());
     });
